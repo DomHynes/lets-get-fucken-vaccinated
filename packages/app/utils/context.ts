2d93e74cbf02e446b77d5fd41b48395d305dcd6c
Dom Hynes <dominichynes@gmail.com>
2021-09-27 00:37:31 +1000

    log token

--- packages/app/utils/context.ts
+++ packages/app/utils/context.ts
@@ -7,6 +7,7 @@ export async function createContext({
 	req,
 }: trpcNext.CreateNextContextOptions) {
 	if (req.headers.authorization) {
+		console.log(req.headers.authorization)
 		const [, token] = req.headers.authorization?.split(' ')
 		if (verify(process.env.JWT_SECRET as string, token)) {
 			return {
