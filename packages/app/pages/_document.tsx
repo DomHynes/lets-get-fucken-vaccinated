1ec4213a2a6c6d94e2123ac47bc5248bdf0dbe01
Dom Hynes <dominichynes@gmail.com>
2021-09-29 21:09:09 +1000

    added seo + twitter tags

--- packages/app/pages/_document.tsx
+++ packages/app/pages/_document.tsx
@@ -36,6 +36,28 @@ export default class Document extends NextDocument {
 						data-domain="letsgetfuckenvaccinated.com"
 						src="https://plausible.io/js/plausible.js"
 					></script>
+
+					<title>let's get fucken VACCINATED</title>
+					<meta name="author" content="@domhynes" />
+
+					<meta property="og:title" content="let's get fucken VACCINATED" />
+					<meta property="og:type" content="website" />
+					<meta
+						property="og:url"
+						content="https://letsgetfuckenvaccinated.com/"
+					/>
+					<meta
+						property="og:image"
+						content="https://letsgetfuckenvaccinated.com/"
+					/>
+
+					<meta name="twitter:card" content="summary_large_image" />
+					<meta name="twitter:creator" content="@domhynes" />
+					<meta name="twitter:title" content="let's get fucken VACCINATED" />
+					<meta
+						name="twitter:image"
+						content="https://letsgetfuckenvaccinated.com/do-i-look-like-i-know-what-a-jpeg-is.png"
+					/>
 				</Head>
 				<body>
 					<Main />
