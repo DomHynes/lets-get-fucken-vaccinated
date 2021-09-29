3e7347121374bd345b334f7e998df8fdde2b5eff
Dom Hynes <dominichynes@gmail.com>
2021-09-29 20:24:37 +1000

    feat: improved spacing and card display

--- packages/app/components/CentreCard.tsx
+++ packages/app/components/CentreCard.tsx
@@ -2,29 +2,39 @@ import tw, { styled } from 'twin.macro'
 import { Appointment } from '.prisma/client'
 
 const Card = styled.div({
-	...tw`p-10 rounded shadow`,
+	...tw`flex flex-col p-10 rounded shadow`,
 })
 
-const CardHeader = styled.h1(tw`text-lg font-bold`)
-const StyledAppointment = styled.span(
-	{
-		...tw`p-3 font-bold text-white rounded bg-gradient-to-br`,
-		variants: {
-			pfizer: {
-				true: tw`from-pink-400 to-blue-600`
-			}, 
-			az: {
-				true: tw`from-red-400 to-blue-600`
-			}
-		}
-	}
+const CardHeader = styled.div(
+	tw`flex flex-col items-start md:space-x-5 md:flex-row`,
 )
-const AppointmentWrapper = styled.div(tw`flex flex-row flex-wrap gap-2 `)
+const CardHeaderText = styled.p(tw`text-lg font-bold`)
+const CardHeaderSubtitle = styled.p(tw``)
+
+const StyledAppointment = styled.span({
+	...tw`flex-shrink-0 p-3 font-bold text-white rounded bg-gradient-to-br`,
+	variants: {
+		pfizer: {
+			true: tw`from-pink-400 to-blue-600`,
+		},
+		az: {
+			true: tw`from-red-400 to-blue-600`,
+		},
+	},
+})
+const AppointmentWrapper = styled.div(
+	tw`flex flex-row gap-2 overflow-x-auto md:flex-wrap`,
+)
 
 const CentreCard = (props: { name: string; appointments: Appointment[] }) => {
 	return (
 		<Card>
-			<CardHeader>{props.name}</CardHeader>
+			<CardHeader>
+				<CardHeaderText>{props.name}</CardHeaderText>{' '}
+				<CardHeaderSubtitle>
+					{props.appointments.length} appointments
+				</CardHeaderSubtitle>
+			</CardHeader>
 
 			<AppointmentWrapper>
 				{props.appointments.map(a => (
--- packages/app/pages/index.tsx
+++ packages/app/pages/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import tw, { styled } from 'twin.macro'
 import { AppRouter } from '..'
 import CentreCard from '../components/CentreCard'
@@ -8,25 +8,35 @@ import { createReactQueryHooks } from '@trpc/react'
 const trpc = createReactQueryHooks<AppRouter>()
 
 const Container = styled.div({
-	...tw`container flex flex-col justify-center min-h-screen gap-10 mx-auto `,
+	...tw`container flex flex-col h-screen gap-10 px-4 mx-auto mt-5 duration-700 ease-in-out transform`,
 })
 
 const Header = styled.h1({
 	...tw`text-5xl font-bold text-center md:text-left md:text-7xl lg:text-8xl`,
 	variants: {
 		hasGradient: {
-			true: tw`text-5xl text-transparent from-pink-400 to-blue-600 bg-gradient-to-br bg-clip-text md:text-8xl lg:text-9xl`,
+			true: tw`text-5xl text-transparent drop-shadow-2xl filter from-pink-400 to-blue-600 bg-gradient-to-br bg-clip-text md:text-8xl lg:text-9xl`,
 		},
 	},
 })
 
-const Subtitle = styled.p(tw`font-semibold `)
+const Subtitle = styled.p(tw`p-2 font-semibold md:text-xl`)
 
 const Fadein = styled.div({
-	...tw`transition-all ease-in-out opacity-0 position[relative] top[300px] transition-duration[400ms]`,
+	...tw`relative flex flex-col space-y-10 duration-700 ease-in-out opacity-0`,
 	variants: {
 		show: {
-			true: tw`top-0 opacity-100`,
+			true: tw`opacity-100`,
+		},
+	},
+})
+
+const TextWrapper = styled.div({
+	...tw`h-screen relative space-y-10 duration-700 ease-in-out transform width[100%]`,
+	variants: {
+		collapsed: {
+			true: tw`top-0`,
+			false: tw`h-0 overflow-visible top-1/2`,
 		},
 	},
 })
@@ -37,23 +47,42 @@ const IndexPage = () => {
 		refetchInterval: 10000,
 	})
 
+	const { withAppointments, withoutAppointments } = useMemo(() => {
+		const withAppointments: typeof data = []
+		const withoutAppointments: typeof data = []
+
+		data?.forEach(d => {
+			d.appointments.length
+				? withAppointments.push(d)
+				: withoutAppointments.push(d)
+		})
+
+		return {
+			withAppointments,
+			withoutAppointments,
+		}
+	}, [data])
+
 	return (
 		<Container>
-			<div>
-				<Header>let's get fucken</Header>
-				<Header hasGradient>VACCINATED</Header>
-			</div>
-
-			<Subtitle>
-				Currently this only shows Pfizer appointments available at State
-				Vaccination Centres over the next month.
-			</Subtitle>
-
-			<Fadein show={!!data}>
-				{data?.map(d => (
-					<CentreCard name={d.name} appointments={d.appointments} />
-				))}
-			</Fadein>
+			<TextWrapper collapsed={!!data}>
+				<div>
+					<Header>let's get fucken</Header>
+					<Header hasGradient>VACCINATED</Header>
+				</div>
+				<Subtitle>
+					This shows Pfizer appointments available at State Vaccination Centres
+					over the next four weeks.
+				</Subtitle>
+				<Fadein show={!!data}>
+					{withAppointments?.map(d => (
+						<CentreCard name={d.name} appointments={d.appointments} />
+					))}
+					{withoutAppointments?.map(d => (
+						<CentreCard name={d.name} appointments={d.appointments} />
+					))}
+				</Fadein>
+			</TextWrapper>
 		</Container>
 	)
 }
